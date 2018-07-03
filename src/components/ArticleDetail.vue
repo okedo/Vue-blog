8b483c90a0f520894f572364e93abeb91e0c4392
Ira Komar <komaririnaandreevna@gmail.com>
2018-07-03 16:23:05 +0300

    added new component articleDetail and link from main page

---
+++ src/components/ArticleDetail.vue
@@ -0,0 +1,24 @@
+<template>
+ <div>
+  <Article v-bind:article = "article" v-bind:key="article.id"></Article>
+</div>
+</template>
+
+<script>
+import Article from "./Article.vue";
+
+export default {
+  props: ['article'],
+  name: "ArticleDetail",
+  components: {
+    Article
+  },
+  data() {
+    console.log(this.$route.params.id);
+    return this.$store.getters.articles[this.$route.params.id];
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
