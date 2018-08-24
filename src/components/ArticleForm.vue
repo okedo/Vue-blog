c7df984a0b0b87579902df80247271def463570f
Viktor Patraboi <vpatr@softserveinc.com>
2018-08-24 14:14:25 +0300

    article form restyle

--- src/components/ArticleForm.vue
+++ src/components/ArticleForm.vue
@@ -1,9 +1,19 @@
 <template>
-  <div>
-    <input v-model="title" type="text" placeholder="Enter aricle name here:"/>
-    <textarea v-model="text" placeholder=""></textarea>
-    <button v-on:click="save">Save</button>
-    <button>Undo</button>
+  <div class="article-form-container">
+    <div class="article-form">
+      <div>
+        <input v-model="title" class="article-form-title-input" type="text" placeholder="Enter aricle name here:"/>
+      </div>
+      <div>
+        <textarea v-model="text" class="article-form-text-input" placeholder=""></textarea>
+      </div>
+      <div>
+        <button v-on:click="save" class="article-form-save article-form-buttons">Save</button>
+      </div>
+      <div>
+        <button v-on:click="undo" class="article-form-undo article-form-buttons">Undo</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -28,10 +38,43 @@ export default {
         text: this.text,
         userId: this.userId
       });
+    },
+    undo() {
+      this.title = "";
+      this.text = "";
     }
   }
 };
 </script>
 
 <style lang="scss" scoped>
+.article-form-container {
+  .article-form {
+    .article-form-title-input {
+      min-width: 300px;
+      height: 40px;
+      padding: 0 10px;
+      font-size: 16px;
+    }
+    .article-form-text-input {
+      margin: 10px;
+      min-width: 300px;
+      width: 70%;
+      font-size: 16px;
+      resize: none;
+      height: 300px;
+    }
+    .article-form-save {
+      margin: 10px;
+    }
+    .article-form-undo {
+    }
+
+    .article-form-buttons {
+      height: 30px;
+      width: 200px;
+      font-size: 20px;
+    }
+  }
+}
 </style>
